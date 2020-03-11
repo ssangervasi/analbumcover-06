13e3046a3a4eec58b950078f0b7958a19d0298a2
Sebastian Sangervasi <villain@harmless.dev>
2020-03-11 12:56:59 -0700

    Tests with no source

---
+++ src/analbumcover.ts
@@ -0,0 +1,13 @@
+import { Spelling } from "./spelling";
+
+const rephrase = (
+	phrase: string,
+	spelling: Spelling,
+	minWordLength: number = 1
+): string | null => {
+	return phrase
+}
+
+export {
+	rephrase
+}
